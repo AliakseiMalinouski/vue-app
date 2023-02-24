9a79f1ff989b59d2c559ccf18b70605bdfad486c
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-02-24 16:50:34 +0100

    Vue app development

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="post" v-for="post in posts" v-bind:key="post.id">
-        <div><strong>Название: </strong>{{ post.title }}</div>
-        <div><strong>Описание: </strong>{{ post.body }}</div>
-  </div>
+    <h3>Cписок постов</h3>
+    <post-item v-for="post in posts" :post="post" v-bind:key="post.id"></post-item>
 </template>
 
 <script>
+import PostItem from './PostItem.vue';
     export default {
         name: "PostList",
+        components: {
+            PostItem
+        },
         props: {
             posts: {
                 type: Array,
